8985f1eb26b7fbc9adb6632b144330300379ce0b
sergio-aguirre12 <sergio2025fwd@gmail.com>
2026-03-03 11:15:54 -0600

    rutas con prefijo lang

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -5,19 +5,17 @@ import LoginPage from '../Pages/Home/LoginPage';
 import RegisterPage from '../Pages/Home/RegisterPage';
 import DashboardAspirante from '../Components/PagPrincipal/DashboardAspirante';
 
-
-
 function Routing() {
   return (
     <Routes>
-
       <Route path='/' element={<Home />} />
-
-
-
-      {/* Agrega más rutas según tu app */}
-
-      <Route path='/' element={<Home />} />
+      <Route path='/:lang' element={<Home />} />
+      <Route path='/:lang/login' element={<LoginPage />} />
+      <Route path='/:lang/register' element={<RegisterPage />} />
+      <Route path='/:lang/mi-perfil' element={<MiPerfilPage />} />
+      <Route path='/:lang/jobs' element={<Home />} />
+      <Route path='/:lang/internships' element={<Home />} />
+      <Route path='/:lang/dashboard-aspirante' element={<DashboardAspirante />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/mi-perfil' element={<MiPerfilPage />} />
