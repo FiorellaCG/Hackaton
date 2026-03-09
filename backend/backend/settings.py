7ca135f4c35e0f93cddda46b383d2ca9f9fc27f3
FiorellaCG <fiochaves1303@gmail.com>
2026-03-09 15:40:37 -0600

    Fix static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR.parent / "dist" / "assets"
+    BASE_DIR.parent / "dist"
 ]
 
 MEDIA_URL = '/media/'
